d17df42f8b1f471c7cc39ce7bed5222bec8e9e69
h4rl <h4rldev@proton.me>
2023-09-12 15:34:26 +0200

    remove boilerplate

--- src/pages/not_found.rs
+++ src/pages/not_found.rs
@@ -1,18 +1,11 @@
 use crate::GoBack;
 use yew::prelude::*;
-use yew_router::prelude::*;
 
 #[function_component(Title)]
 pub fn title() -> Html {
     html! {
         <h1>{ "404" }</h1>
     }
-}
-
-pub fn back_listener(nav: Navigator) -> Callback<MouseEvent> {
-    Callback::from(move |_| {
-        nav.back();
-    })
 }
 
 #[function_component(NotFound)]
