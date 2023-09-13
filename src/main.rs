f98f789bfa8b3ec00d55aee9c8ae9b27f18e08ea
h4rl <h4rldev@proton.me>
2023-09-13 16:01:07 +0200

    Implement Display for Route and make it more funny

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use pages::{about::About, blog::Blog, index::Index, not_found::NotFound};
+use std::fmt;
 use yew::prelude::*;
 use yew::Renderer;
 use yew_router::prelude::*;
@@ -18,6 +19,17 @@ pub enum Route {
     NotFound,
 }
 
+impl fmt::Display for Route {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Route::Index => write!(f, "Home"),
+            Route::About => write!(f, "About"),
+            Route::Blog => write!(f, "Blog"),
+            Route::NotFound => write!(f, "404"),
+        }
+    }
+}
+
 fn switch(routes: Route) -> Html {
     match routes {
         Route::Index => html! { <Index /> },
@@ -31,14 +43,14 @@ fn switch(routes: Route) -> Html {
 pub struct GoToProps {
     #[prop_or(Route::Index)]
     pub route: Route,
-    #[prop_or(("Go to".to_string(), "?".to_string()))]
-    pub message: (String, String),
+    #[prop_or(("Go to ".to_string(), Route::Index.to_string(), "?".to_string()))]
+    pub message: (String, String, String),
 }
 
 #[function_component(GoTo)]
 fn goto(props: &GoToProps) -> Html {
     let route: Route = props.route.clone();
-    let message: (String, String) = props.message.clone();
+    let message: (String, String, String) = props.message.clone();
     let navigator = use_navigator().unwrap();
     let push_route = route.clone();
     let onclick = Callback::from(move |_| {
@@ -48,31 +60,31 @@ fn goto(props: &GoToProps) -> Html {
         Route::About => {
             html! {
             <p>{ message.0.as_str() }
-                <a href="" target="_self" {onclick} class="hover:text-teal-200">{ "Go to About?" }</a>
-                { message.1.as_str() }
+                <a href="" target="_self" {onclick} class="hover:text-teal-200">{ message.1.as_str() }</a>
+                { message.2.as_str() }
             </p>
             }
         }
         Route::Blog => {
             html! {
                 <p>{ message.0.as_str() }
-                    <a href="" target="_self" {onclick} class="hover:text-teal-200">{ "Blog?" }</a>
-                    { message.1.as_str() }
+                    <a href="" target="_self" {onclick} class="hover:text-teal-200">{ message.1.as_str() }</a>
+                    { message.2.as_str() }
                 </p>
             }
         }
         Route::Index => {
             html! {
                 <p>{ message.0.as_str() }
-                    <a href="" target="_self" {onclick} class="hover:text-teal-200">{ "Home" }</a>
-                    { message.1.as_str() }
+                    <a href="" target="_self" {onclick} class="hover:text-teal-200">{ message.1.as_str() }</a>
+                    { message.2.as_str() }
                 </p>
             }
         }
         _ => {
             html! {
-                <p>{ "Go to " }
-                    <a href="" target="_self" {onclick} class="hover:text-teal-200">{ "???" }</a>
+                <p>{ message.0.as_str() }
+                    <a href="" target="_self" {onclick} class="hover:text-teal-200">{ message.1.as_str() }</a>
                 </p>
             }
         }
