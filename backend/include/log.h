3461fbcbf3ab8527e71d63223e4352f55e1db256
h4rl <h4rldev@proton.me>
2025-01-21 00:18:51 +0100

    chore: remove backend

--- backend/include/log.h
+++
@@ -1,8 +0,0 @@
-#ifndef LOG_H_IMPLEMENTATION
-#define LOG_H_IMPLEMENTATION
-
-typedef enum { Error, Warning, Info, Debug } LogLevel;
-
-int flscio_log(LogLevel level, char *fmt, ...);
-
-#endif // !LOG_H_IMPLEMENTATION
